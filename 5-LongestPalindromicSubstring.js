afb6185272466edbb66d9f845a8ee8d55fde8868
huangjue <huangjue@meituan.com>
2017-03-22 11:17:59 +0800

    finish 5

---
+++ 5-LongestPalindromicSubstring.js
@@ -0,0 +1,59 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    let result = '';
+    let length = s.length;
+    let isOdd = false;
+
+    if (!s || s.length === 1) {
+        return s;
+    }
+
+    result = s[0];
+
+    for (let i = 0; i < length; i++) {
+        let j = 0.5;
+        let front = Math.ceil(i - j);
+        let end = Math.ceil(i + j);
+        let flag = false;
+        while (s[front] && s[end]) {
+            let strLength = j * 2 + 1;
+            if (s[front] === s[end]) {
+                if (strLength > result.length && (!flag || strLength % 2 === (isOdd ? 1 : 0))) {
+                    result = s.slice(front, end + 1);
+                }
+                if (flag) {
+                    j += 1;
+                } else {
+                    j += 0.5;
+                }
+            } else {
+                if (flag) {
+                    break;
+                } else {
+                    flag = true;
+                    // 预测结果是奇数还是偶数
+                    if (strLength % 2 === 0) {
+                        isOdd = true;
+                    } else {
+                        isOdd = false;
+                    }
+                    j += 0.5;
+                }
+            }
+
+
+            front = Math.ceil(i - j);
+            end = Math.ceil(i + j);
+        }
+    }
+
+    return result;
+};
+
+const pa = '"aaabaaaa"';
+const param = "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa";
+
+console.log(longestPalindrome(pa));
